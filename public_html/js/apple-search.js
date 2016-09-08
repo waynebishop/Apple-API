e6a9cf0408a8dbbdfc95fdf3eddf9e84aa4fb62e
Wayne Bishop <bishopwb66@gmail.com>
2016-09-09 11:43:38 +1200

    completed basic project

--- public_html/js/apple-search.js
+++ public_html/js/apple-search.js
@@ -34,6 +34,66 @@ $(document).ready(function(){
 			},
 			success:function(dataFromServer) {
 				console.log(dataFromServer);
+
+				// Clear old results
+				$('#results').html('');
+				
+				//If there is no results
+				if(dataFromServer.resultCount == 0) {
+					//Display no result message
+					$('#results').html('Sorry, we could not find '+$('#apple-search select option:selected').html()+'s for '+searchQuery);
+
+					return;
+
+				}
+
+				//Loop over the results
+				for(var i = 0; i<dataFromServer.results.length; i++) {
+
+					//Put row into variable
+					var product = dataFromServer.results[i];
+
+					//Prepare a variable to hold all the html
+					var div = $('<div class="search-result">');
+
+					// Inject the heading and icon into the result div
+					$(div).append('<h2><a target="blank" href="'+product.artistViewUrl+'">'+product.artistName+'</a></h2>');
+					$(div).append('<img src="'+product.artworkUrl100+'"Alt="">');
+					$(div).append('<p>$'+(product.collectionPrice || product.price)+' '+product.currency+'</p>');
+
+					// If this product has a description
+					if(product.shortDescription){
+						$(div).append('<p>'+product.shortDescription+'...</p>');
+					}
+
+					// If the product has an audio preview
+					switch( product.kind ){
+
+						//Audio
+						case 'song':
+						case 'audiobook':
+						case 'mix':
+							// Append an audio element to play preview
+							$(div).append('<audio preload="none" controls src="'+product.previewUrl+'">');
+
+						break;
+						
+						//Video
+						case 'musicVideo':
+						case 'tv-episode':
+							$(div).append('<video preload="none" controls=src="'+product.previewUrl+'">');	
+
+					}
+
+
+					// Close the div
+					$('#results').append(div);
+
+
+
+
+				}				
+
 			},
 			error:function(){
 
